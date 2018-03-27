1e08399837f9d43ae0087c6a05ce2020cb78fb85
neex-io <neex.io@protonmail.com>
2018-03-26 20:58:21 -0700

    Add HTML to return

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,18 @@ from app import app
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello world!"
+    # mock user
+    user = {"username" : "Neexiey"}
+
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>''' # you can return HTML too!
 
 # In Flask, handlers for the application routes are written as Python functions, called
 # view functions. View functions are mapped to one or more route URLs so that Flask knows
@@ -11,3 +22,8 @@ def index():
 
 # A decorator modifies the function that follows it. A common pattern with decorators
 # is to use them to register functions as callbacks for certain events.
+
+# Creating mock objects is a useful technique that allows you to concentrate on one part of the
+# application without having to worry about other parts of the system that don’t exist yet. I want
+# to design the home page of my application, and I don’t want the fact that I don’t have a user
+# system in place to distract me, so I just make up a user object so that I can keep going.
--- microblog.py
+++ microblog.py
@@ -1,3 +1,5 @@
 from app import app
 
-# export FLASK_APP=microblog.py
+# To run the application you set the FLASK_APP=microblog.py in your terminal session, and
+# then execute flask run . This starts a web server with the application, which you can open
+# by typing the http://localhost:5000/ URL in your web browser’s address bar.
