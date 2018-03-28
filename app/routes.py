af575f24f8f42f8cba572839a3a1f876e0ded502
neex-io <neex.io@protonmail.com>
2018-03-27 20:36:48 -0700

    Accept form input

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template # comes with flask, it is a function
+from flask import render_template, flash, redirect # comes with flask, it is a function
 from app import app
 from app.forms import LoginForm
 
@@ -27,9 +27,13 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login requested for user {}, remember_me={}".format(
+            form.username.data, form.remember_me.data))
+        return redirect("/index")
     return render_template("login.html", title="Sign In", form=form)
 # In Flask, handlers for the application routes are written as Python functions, called
 # view functions. View functions are mapped to one or more route URLs so that Flask knows
